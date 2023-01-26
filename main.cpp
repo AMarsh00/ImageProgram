f22743d42ad92f871603a6ee29a3d773ecf58a0c
ajkmcurator <ajkm.curator@gmail.com>
2023-01-26 02:58:27 -0700

    Make more readable

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
 	std::cout << clock() << std::endl;
 
-	BitmapImage image("D:\\ImageProgram\\");
+	BitmapImage image("D:\\ImageProgram\\BITMAP\\");
 	// BitmapImage image("D:\\ImageProgram\\", "customMean.bmp", "customMedian.bmp", "customMode.bmp");
 	// You can also set the output files (this example will put the mean image in D:\\ImageProgram\\customMean.bmp)
 	
@@ -14,7 +14,7 @@ int main() {
 
 	std::cout << clock() << std::endl;
 
-	JPEGImage image2("D:\\ImageProgram\\", "JPEG\\jpeg_mean.bmp", "JPEG\\jpeg_median.bmp", "JPEG\\jpeg_mode.bmp");
+	JPEGImage image2("D:\\ImageProgram\\JPEG\\", "jpeg_mean.bmp", "jpeg_median.bmp", "sjpeg_mode.bmp");
 	// JPEGImage image2("D:\\ImageProgram\\");
 	// You can also not set specific output files, but if you're using multiple instances of an Image child, you want to
 	// Do note that all outputs must be .bmp files, as I haven't figured out how to write to JPG/GIF/etc. yet
